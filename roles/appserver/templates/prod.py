c4be8eb1ff776de7246caf10d1e4f61328c3024a
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-05-04 17:53:30 +0200

    Use environment variables instead of django settings file

--- roles/appserver/templates/prod.py
+++
@@ -1,6 +0,0 @@
-DATABASE_URL = 'postgresql:///var/lib/postgresql/socket.md:{{ project_name }}'
-MEDIA_ROOT = '{{ root_dir }}/media'
-STATIC_ROOT = '{{ root_dir }}/static'
-MEDIA_URL = 'http://{{ domain }}/media/'
-STATIC_URL = 'http://{{ domain }}/static/'
-ALLOWED_HOSTS = ('{{ domain }}',)
