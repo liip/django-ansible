25dc6a625678539b7546347489bdab3bfb78008b
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-05-23 15:33:51 +0200

    lets encrypt: fixed certificate renew

--- roles/webserver/templates/update-certs.sh
+++ roles/webserver/templates/update-certs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-letsencrypt certonly --webroot --webroot-path /tmp/letsencrypt -d {{ domain }} {% if use_www %}-d www.{{ domain }}{% endif %}
+letsencrypt certonly --webroot --webroot-path /tmp/letsencrypt -d {{ domain }} {% if use_www %}-d www.{{ domain }}{% endif %} --keep-until-expiring -n
 sudo systemctl reload nginx
