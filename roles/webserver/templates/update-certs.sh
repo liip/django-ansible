02ea7c1d9b3f9b6b30e60dba9ebff43b31d7e73e
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-05-23 11:22:02 +0200

    Domain: make www. configurable

--- roles/webserver/templates/update-certs.sh
+++ roles/webserver/templates/update-certs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-letsencrypt certonly --webroot --webroot-path /tmp/letsencrypt -d {{ domain }} -d www.{{ domain }}
+letsencrypt certonly --webroot --webroot-path /tmp/letsencrypt -d {{ domain }} {% if use_www %}-d www.{{ domain }}{% endif %}
 sudo systemctl reload nginx
