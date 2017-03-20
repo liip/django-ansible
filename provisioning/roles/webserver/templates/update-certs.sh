eb07bd817c2aafbc09d615d6deb7e252e031a6c0
Fabio Anderegg <fabio.anderegg@liip.ch>
2017-03-20 13:24:25 +0100

    Lets encrypt: create tmp directory for lets encrypt

--- provisioning/roles/webserver/templates/update-certs.sh
+++ provisioning/roles/webserver/templates/update-certs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+mkdir -p /tmp/letsencrypt/
 letsencrypt certonly --webroot --webroot-path /tmp/letsencrypt -d {{ domain }} {% if use_www %}-d www.{{ domain }}{% endif %} --keep-until-expiring -n
 sudo systemctl reload nginx
