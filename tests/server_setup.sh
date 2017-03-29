ebe9252dd2546cc73446058826ece98eb7fae60f
Fabio Anderegg <fabio.anderegg@liip.ch>
2017-03-29 18:27:10 +0200

    tests: updated server setup script

--- tests/server_setup.sh
+++ tests/server_setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-apt update
-apt install python -y
-adduser manager --disabled-password -q
+useradd manager -m -s /bin/bash
 echo "manager ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-sudo -u manager bash -c "mkdir ~/.ssh"
+sudo -u manager bash -c "mkdir /home/manager/.ssh"
 cp /root/.ssh/authorized_keys /home/manager/.ssh/authorized_keys
 chown manager:manager /home/manager/.ssh/authorized_keys
+apt update
+apt install python -y
