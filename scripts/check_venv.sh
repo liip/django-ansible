4c86e1da4a1b8db4cfc039f2c17e46a6bbe99eda
Adrian Schlegel <adrian@liip.ch>
2018-08-30 16:44:21 +0200

    Switch to python3 and latest ansible version
    Since ansible >=2.5 supports python >=3.5 there is no longer the need to
    use either python2.7 or an outdated ansible version.

--- scripts/check_venv.sh
+++ scripts/check_venv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ ! -d "venv" ]; then
-    virtualenv -p python2 venv/
-    venv/bin/pip install ansible~=2.1.1.0
+    virtualenv -p python3 venv/
+    venv/bin/pip install ansible
 fi
